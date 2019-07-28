c05384967af21c21ab121a33490fae78bd63afbf
Saki Cake <saki@cakeforge.co>
2019-07-27 23:54:57 -0700

    [create-user] Add register user

--- client/app/page-components/top-nav-bar/top-nav-bar.component.ts
+++ client/app/page-components/top-nav-bar/top-nav-bar.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener} from '@angular/core';
 import { UserManagementService } from '../../modules/user-management/user-management.service';
 import { CommonUXService } from '../../modules/common-ux/common-ux.service';
-import { IUserViewModel } from 'client/app/app.view-models';
+import { IUserViewModel, ILogInViewModel, IServerResponse } from 'client/app/app.view-models';
 
 @Component({
   selector: 'top-nav-bar',
@@ -10,8 +10,12 @@ import { IUserViewModel } from 'client/app/app.view-models';
 })
 export class TopNavBarComponent implements OnInit {
     public user: IUserViewModel;
+    public newUser: IUserViewModel = {} as IUserViewModel;
+    public logInModel: ILogInViewModel = {} as ILogInViewModel;
+    public showLoginForm: boolean = true;
+    public justCheckedCreateUserForm: boolean = false;
 
-    constructor(private commonUXService: CommonUXService, private userService: UserManagementService){
+    constructor(private commonUxService: CommonUXService, private userService: UserManagementService) {
     }
 
     ngOnInit() {
@@ -21,7 +25,7 @@ export class TopNavBarComponent implements OnInit {
           this.user = res;
         },
         error: err => {
-          this.commonUXService.showDangerToast('Unable to get user data.');
+          this.commonUxService.showDangerToast('Unable to get user data.');
           console.error(err);
         },
         complete: () => {
@@ -33,5 +37,49 @@ export class TopNavBarComponent implements OnInit {
     }
     public logOut(): void {
       this.userService.logOut();
+    }
+    public createUser(): void {
+      if (!this.validateNewUser()) {
+        this.commonUxService.showWarningToast('Please address highlighted errors.');
+        return;
+      }
+
+      this.userService.createUser(this.newUser).subscribe(
+        (res: IServerResponse) => {
+            if (res.success) {
+              this.commonUxService.showSuccessToast('Congratulations! Your account has been created.');
+              this.showLoginForm = true;
+            } else {
+              this.commonUxService.showDangerToast('Unable to create account.');
+              console.error(res.error);
+            }
+          },
+          error => {
+            this.commonUxService.showDangerToast('Unable to create account.');
+            console.error(error);
+          }
+        );
+    }
+
+    private validateNewUser(): boolean {
+      let isValid: boolean = true;
+
+      if (!this.newUser.emailAddress) {
+        isValid = false;
+      }
+      if (!this.newUser.userName) {
+        isValid = false;
+      }
+      if (!this.newUser.password) {
+        isValid = false;
+      }
+      if (!this.newUser.passwordConfirm) {
+        isValid = false;
+      }
+      if (this.newUser.password !== this.newUser.passwordConfirm) {
+        isValid = false;
+      }
+      this.justCheckedCreateUserForm = true;
+      return isValid;
     }
 }
