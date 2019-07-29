a5a43d0d30b17f71fb61edf23329f15934d06934
Saki Cake <saki@cakeforge.co>
2019-07-29 16:53:40 -0700

    [Feature] Slide-out login/registration panel
    Change navbar color and links
    More to come...

---
+++ client/app/modules/common-ux/components/slide-panel/slide-panel.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input, OnInit, HostBinding } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+type SlideDirection = 'left' | 'right';
+
+// A fleshed out and re-designed version of the following experiment:
+// https://medium.com/@asm/animated-slide-panel-with-angular-e985ad646f9
+
+@Component({
+  selector: 'common-ux-slide-panel',
+  styleUrls: [ './slide-panel.component.css' ],
+  templateUrl: './slide-panel.component.html',
+  animations: [
+    trigger('slide', [
+      state('visibleLeft', style({ transform: 'translateX(0)' })),
+      state('hiddenLeft', style({ transform: 'translateX(50%)' })),
+      state('visibleRight', style({ transform: 'translateX(-50%)' })),
+      state('hiddenRight', style({ transform: 'translateX(0)' })),
+      transition('* => *', animate(300))
+    ])
+  ]
+})
+export class SlidePanelComponent implements OnInit {
+  @Input() slideDirection: SlideDirection = 'left';
+  @Input() set paneVisible(isVisible: boolean) {
+    this._paneVisible = isVisible;
+    if (isVisible) {
+      if (this.slideDirection === 'left') {
+        this.paneAnimationState = 'visibleLeft';
+      } else if (this.slideDirection === 'right') {
+        this.paneAnimationState = 'visibleRight';
+      }
+      this.setHighZIndexClass = true;
+      this.setLowZIndexClass = false;
+    } else {
+      if (this.slideDirection === 'left') {
+        this.paneAnimationState = 'hiddenLeft';
+      } else if (this.slideDirection === 'right') {
+        this.paneAnimationState = 'hiddenRight';
+      }
+      setTimeout(() => {
+        // This needs to be delayed so we don't see things
+        // through the menu when it's still being animated
+        this.setLowZIndexClass = true;
+        this.setHighZIndexClass = false;
+      }, 300);
+    }
+  }
+  get paneVisible() {
+    return this._paneVisible;
+  }
+  private _paneVisible: boolean = false;
+  public paneAnimationState: string = 'hiddenLeft';
+
+  // These classes can be found in common-ux.css
+  // I'd like to move them to this component's css file, but the targeting isn't working from this level (yet).
+  // Will try to revisit later.
+  @HostBinding('class.slide-panel-hide') setHidePanelClass: boolean = true;
+  @HostBinding('class.slide-panel-high-z-index') setHighZIndexClass: boolean = false;
+  @HostBinding('class.slide-panel-low-z-index') setLowZIndexClass: boolean = false;
+
+  ngOnInit() {
+    // The panel itself needs to be hidden on page load so users don't see it animating away.
+    // For some reason, it starts out displayed before hiding itself.
+    setTimeout(() => {
+      this.setHidePanelClass = false;
+    }, 400);
+  }
+}
