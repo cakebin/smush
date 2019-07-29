1ec2a9d10fe8c0733cde83ea6a7f808c9e08ee4c
Jordan Medina <jordan@humblebundle.com>
2019-07-28 19:26:54 -0700

    [user-auth] Add user authentication with jwt tokens

--- server/db/user.go
+++ server/db/user.go
@@ -1,90 +1,154 @@
 package db
 
 import (
-	"time"
+  "time"
 )
 
 // User represents a recorded user for our Smash Ultimate Online tracker app
 type User struct {
-	UserID               *int      `json:"userId,omitempty"`
-	UserName             string    `json:"userName"`
-	EmailAddress         string    `json:"emailAddress"`
-	DefaultCharacterName *string   `json:"defaultCharacterName,omitempty"`
-	DefaultCharacterGsp  *int      `json:"defaultCharacterGsp,omitempty"`
-	Created              time.Time `json:"created"`
+  UserID               *int       `json:"userId,omitempty"`
+  UserName             string     `json:"userName"`
+  EmailAddress         string     `json:"emailAddress"`
+  DefaultCharacterName *string    `json:"defaultCharacterName,omitempty"`
+  DefaultCharacterGsp  *int       `json:"defaultCharacterGsp,omitempty"`
+  Created              time.Time  `json:"created"`
+  Password             *string    `json:"password,omitempty"`
+  HashedPassword       *string    `json:"hashedPassword,omitempty"`
+  RefreshToken         *string    `json:"refreshToken,omitempty"`
 }
 
-// UserResponse represents an interaction with our database
-// regarding operations related to the Users table
-type UserResponse struct {
-	Success bool  `json:"success"`
-	Error   error `json:"error"`
-}
+
 
 // GetUserByID gets a specific user by its id in the Users table
 func (db *DB) GetUserByID(id int) (*User, error) {
-	row := db.QueryRow(`SELECT * FROM users WHERE user_id = $1`, id)
-	user := new(User)
-	err := row.Scan(
-		&user.UserID,
-		&user.UserName,
-		&user.EmailAddress,
-		&user.DefaultCharacterName,
-		&user.DefaultCharacterGsp,
-		&user.Created,
-	)
+  sqlStatement := `
+  SELECT
+    user_id,
+    user_name,
+    email_address,
+    default_character_name,
+    default_character_gsp,
+    created
+  FROM
+    users
+  WHERE
+    user_id = $1
+  `
+  row := db.QueryRow(sqlStatement, id)
+  user := new(User)
+  err := row.Scan(
+    &user.UserID,
+    &user.UserName,
+    &user.EmailAddress,
+    &user.DefaultCharacterName,
+    &user.DefaultCharacterGsp,
+    &user.Created,
+  )
 
-	if err != nil {
-		return nil, err
-	}
+  if err != nil {
+    return nil, err
+  }
 
-	return user, nil
+  return user, nil
 }
+
+
+// GetUserCredByEmail gets a user's auth related information
+// by their email; used for user authentication
+func (db *DB) GetUserCredByEmail(email string) (*User, error) {
+  sqlStatement := `
+  SELECT
+    user_id,
+    email_address,
+    hashed_password,
+    refresh_token
+  FROM
+   users
+  WHERE
+    email_address = $1`
+  row := db.QueryRow(sqlStatement, email)
+  user := new(User)
+  err := row.Scan(
+    &user.UserID,
+    &user.EmailAddress,
+    &user.HashedPassword,
+    &user.RefreshToken,
+  )
+
+  if err != nil {
+    return nil, err
+  }
+
+  return user, nil
+}
+
+
+// UpdateUserRefreshTokenByID updates a user's stored jwt
+// refresh token by their email; used for user authentication
+func (db *DB) UpdateUserRefreshTokenByID(token string, id int) (bool, error) {
+  sqlStatement := `
+  UPDATE users SET
+    refresh_token = $1
+  WHERE
+    user_id = $2`
+  _, err := db.Exec(
+    sqlStatement,
+    token,
+    id,
+  )
+
+  if err != nil {
+    return false, err
+  }
+
+  return true, nil
+}
+
 
 // UpdateUser updates an existing entry in the Users table
 func (db *DB) UpdateUser(user User) (bool, error) {
-	sqlStatement := `
-	UPDATE users SET 
-		user_name = $1,
-		default_character_name = $2,
-		default_character_gsp = $3
-	WHERE user_id = $4`
-	_, err := db.Exec(
-		sqlStatement,
-		user.UserName,
-		user.DefaultCharacterName,
-		user.DefaultCharacterGsp,
-		user.UserID,
-	)
+  sqlStatement := `
+  UPDATE users SET 
+    user_name = $1,
+    default_character_name = $2,
+    default_character_gsp = $3
+  WHERE user_id = $4`
+  _, err := db.Exec(
+    sqlStatement,
+    user.UserName,
+    user.DefaultCharacterName,
+    user.DefaultCharacterGsp,
+    user.UserID,
+  )
 
-	if err != nil {
-		return false, err
-	}
+  if err != nil {
+    return false, err
+  }
 
-	return true, nil
+  return true, nil
 }
 
 // CreateUser adds a new entry to the Users table in our database
 func (db *DB) CreateUser(user User) (bool, error) {
-	sqlStatement := `
-	INSERT INTO users (
-		user_name,
-		email_address,
-		default_character_name,
-		default_character_gsp,
-	)
-	VALUES ($1, $2, $3, $4)`
-	_, err := db.Exec(
-		sqlStatement,
-		user.UserName,
-		user.EmailAddress,
-		user.DefaultCharacterName,
-		user.DefaultCharacterGsp,
-	)
+  sqlStatement := `
+  INSERT INTO users (
+    user_name,
+    email_address,
+    hashed_password,
+    refresh_token
+  )
+  VALUES ($1, $2, $3, $4)`
+  _, err := db.Exec(
+    sqlStatement,
+    user.UserName,
+    user.EmailAddress,
+    user.HashedPassword,
+    user.RefreshToken,
+  )
 
-	if err != nil {
-		return false, err
-	}
+  if err != nil {
+    return false, err
+  }
 
-	return true, nil
+  return true, nil
 }
