ef4eb52688ed1194ae93f8aa4c8da58c4aa0f3d3
Saki Cake <saki@cakeforge.co>
2019-08-26 23:19:51 -0700

    [Feature] Skeleton cards for load state

---
+++ client/app/modules/match-management/components/skeleton-match-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'skeleton-match-card',
+  templateUrl: './skeleton-match-card.component.html',
+  styleUrls: ['./match-card.component.css']
+})
+export class SkeletonMatchCardComponent {
+}
